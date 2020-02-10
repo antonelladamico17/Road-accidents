2f3c915b6a012778177220fe5984f34969f25ae6
antonelladamico17 <60288164+antonelladamico17@users.noreply.github.com>
2020-02-10 09:53:18 +0100

    Add files via upload
    up R file

---
+++ R/SERVER.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(plotly)
+library(jsonlite)
+
+
+data <-
+  fromJSON("https://www.dati.lombardia.it/resource/kfsx-gizw.json")
+
+data$anno_incidente <- as.numeric(data$anno_incidente)
+# Shiny App Server
+
+shinyServer(function(input, output, session) {
+  # Render plot in main panel
+
+
+  output$plot  <- renderPlotly({
+    for (a in input$year) {
+      if (a == input$year) {
+        mean = year_dead(a)
+
+        plot_ly(
+          data = data,
+          x = ~ data$provincia,
+          y = ~ data$totale_morti,
+          name = "Dead",
+          type = 'scatter',
+          mode = "markers",
+          marker = list(color = "#c71585")
+        ) %>%
+
+
+          layout(
+            title = sprintf(
+              "Dead people divided by district in %d",
+              data$anno_incidente
+            ),
+            yaxis = list(title = "Mean of dead people"),
+            xaxis = list(title = "District"),
+            margin = list(l = 100)
+          )
+      }
+      break
+
+    }
+    # distdead <- input$district
+    # yeardead <- input$year
+
+    # for (a in input$year) {
+    #   if (a == input$year) {
+    #     #print(distdead)
+    #   }
+    # }
+
+
+  })
+
+})
