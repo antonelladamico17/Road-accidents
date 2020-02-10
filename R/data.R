d94c3bb3ed3470b10c37e408f75f3ea2470ae083
antonelladamico17 <60288164+antonelladamico17@users.noreply.github.com>
2020-02-10 19:42:51 +0100

    Add files via upload
    up new file w/ function to import data from Json

--- R/data.R
+++ R/data.R
@@ -5,13 +5,15 @@ library(shiny)
 library(dplyr)
 
 ### Dataframe to make the analysis
+getData <- function(){
+  data <- jsonlite::fromJSON("https://www.dati.lombardia.it/resource/kfsx-gizw.json")
+  data$anno_incidente <- as.numeric(data$anno_incidente)
+  data$numero_incidenti <- as.numeric(data$numero_incidenti)
+  data$totale_morti <- as.numeric(data$totale_morti)
+  data$totale_feriti <- as.numeric(data$totale_feriti)
+  return(data)
+}
 
-data <-
-  fromJSON("https://www.dati.lombardia.it/resource/kfsx-gizw.json")
 
 ##### Trasformation of chr into num ######
-data$anno_incidente <- as.numeric(data$anno_incidente)
-data$numero_incidenti <- as.numeric(data$numero_incidenti)
-data$totale_morti <- as.numeric(data$totale_morti)
-data$totale_feriti <- as.numeric(data$totale_feriti)
 
